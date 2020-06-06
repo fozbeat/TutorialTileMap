87f21047f2f2fc6040655d2f7d3f9a0bfa7ea66a
fozbeat <66278370+fozbeat@users.noreply.github.com>
2020-06-07 01:28:22 +0530

    In-game basic menu added
    In game basic menu for map editor added.

---
+++ Assets/UI/Scripts/AutoScaleVerticalMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScaleVerticalMenu : MonoBehaviour
+{
+    public float childHeight = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScaleSize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ScaleSize()
+    {
+        Vector2 size = this.GetComponent<RectTransform>().sizeDelta;
+        size.y = this.transform.childCount * childHeight;
+        this.GetComponent<RectTransform>().sizeDelta = size;
+    }
+}
---
+++ Assets/UI/Scripts/ToggleInGamePanels.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleInGamePanels : MonoBehaviour
+{
+    public GameObject tilesPanel;
+
+    public void TogglePanel()
+    {
+        if(tilesPanel != null)
+        {
+            bool isActive = tilesPanel.activeSelf;
+            tilesPanel.SetActive(!isActive);
+        }
+    }
+}
